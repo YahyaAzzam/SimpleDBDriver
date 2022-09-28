518b9f8949e9cd3d21e5d92086cc9e1212d4ccfc
YahyaAzzam <96234204+YahyaAzzam@users.noreply.github.com>
2022-09-29 01:50:07 +0200

    add test

--- src/test/java/DriverTest.java
+++ src/test/java/DriverTest.java
@@ -3,11 +3,12 @@ package test.java;
 //import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-//import main.java.Driver;
+import main.java.Driver;
 
 public class DriverTest {
     @Test
-    void test() {
-
+    void test() throws Exception {
+        Driver driver = new Driver();
+        driver.createDatabase("C:\\Users\\yahya\\OneDrive\\Desktop\\coding\\code-blocks\\Check-in-flight-system\\SimpleFSDB\\tests\\Check-in-schema.json");
     }
 }
