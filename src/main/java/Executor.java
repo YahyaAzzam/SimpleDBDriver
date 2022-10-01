73a09530bacb4e150f4ddcebfa62db4869c10a0e
MahmoudGoda2003 <95986689+MahmoudGoda2003@users.noreply.github.com>
2022-10-01 05:45:00 +0200

    still working on it

--- src/main/java/Executor.java
+++ src/main/java/Executor.java
@@ -5,24 +5,37 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Properties;
 
 public class Executor {
-    /**
-     * @param command
-     *      command entered
+    private String databasePath;
+    private String config;
+    static private Executor instance;
+    /*
      * @return
      *      Returns the api of the main and null if something wrong happened with the database call
      */
+    private Executor() throws IOException {
+        loadProperties();
+    }
+    public static Executor getInstance() throws IOException {
+        if (instance == null) {
+            instance = new Executor();
+        }
+        return instance;
+    }
+    private void loadProperties() throws IOException {
+        FileReader fileReader = new FileReader("driver_properties.properties");
+        Properties properties = new Properties();
+        properties.load(fileReader);
+        this.databasePath = properties.getProperty("databasePath");
+        this.config = String.format("%s ", properties.getProperty("config"));
+        fileReader.close();
+    }
     public JSONObject execute(final String command) throws InternalErrorException {
         try {
-            FileReader fileReader = new FileReader("driver_properties.properties");
-            Properties properties = new Properties();
-            properties.load(fileReader);
-            String databasePath = properties.getProperty("databasePath");
-            String config = String.format("%s ", properties.getProperty("config"));
-            fileReader.close();
             final Process pr = Runtime.getRuntime().exec(new String[]{config, databasePath, command});
             BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
             return new JSONObject(input.readLine());
